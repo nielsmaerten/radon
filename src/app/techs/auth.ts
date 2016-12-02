db7a40d8c0016d39c7b37a0affb272d478f6c5db
Niels Maerten <niels@maerten.be>
2016-12-02 23:25:50 +0100

    Add auth service

---
+++ src/app/techs/auth.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../../typings/index.d.ts" />
+import * as firebase from 'firebase';
+
+export class AuthService {
+  private database: firebase.database.Database;
+
+
+  /** @ngInject */
+  constructor() {
+    //
+  }
+
+  public getUserId() {
+    // todo
+    //throw 'not implemented';
+  }
+};
