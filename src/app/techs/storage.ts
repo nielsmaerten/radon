e3d8a68e42b8a95dc7c0594a44943497645858cf
Niels Maerten <niels@maerten.be>
2016-12-03 14:51:21 +0100

    Add fetch/save functionality for Stories in Storageservice

--- src/app/techs/storage.ts
+++ src/app/techs/storage.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../../typings/index.d.ts" />
-import { EncryptedStory } from '../model/radon';
+import { EncryptedStory, Story } from '../model/radon';
 import { FirebaseService } from './firebase';
 import { AuthService } from './auth';
 import * as firebase from 'firebase';
@@ -7,18 +7,49 @@ import * as moment from 'moment';
 
 export class StorageService {
   private database: firebase.database.Database;
-  private AuthService: AuthService;
+  private authService: AuthService;
+  private $q: angular.IQService;
+  private stories: any;
 
 
   /** @ngInject */
-  constructor(FirebaseService: FirebaseService, AuthService: AuthService) {
+  constructor(FirebaseService: FirebaseService, AuthService: AuthService, $q: angular.IQService) {
     this.database = FirebaseService.getDatabase();
-    this.AuthService = AuthService;
+    this.authService = AuthService;
+    this.$q = $q;
+    this.stories = {};
   }
 
-  public saveStory(EncryptedStory: EncryptedStory) {
-    let dateString = moment(EncryptedStory.Date).format('YYYYMMDD');
-    let testUser = this.AuthService.getUserId();
-    this.database.ref(`users/${testUser}/entries/${dateString}`).set(EncryptedStory);
+  public saveStory(story: EncryptedStory) {
+    this.database.ref(this.getStoryRef(story.Date)).set(story);
+  }
+
+  public fetchStory(date: Date): angular.IPromise<EncryptedStory> {
+    let deferred: angular.IDeferred<EncryptedStory> = this.$q.defer<EncryptedStory>();
+    let story = this.tryGetStoryFromCache(date);
+    if (story) {
+      deferred.resolve(story);
+    } else {
+      this.database.ref(this.getStoryRef(date)).on('value', snapshot => {
+        let story: EncryptedStory = snapshot.val();
+        this.stories[this.getDateRef(story.Date)] = story;
+        deferred.resolve(story);
+      });
+    }
+    return deferred.promise;
+  }
+
+  private tryGetStoryFromCache(date: Date) {
+    return this.stories[this.getDateRef(date)];
+  }
+
+  private getStoryRef(storyDate: Date) {
+    let dateRef = this.getDateRef(storyDate);
+    let userId = this.authService.getUserId();
+    return `users/${userId}/entries/${dateRef}`;
+  }
+
+  private getDateRef(date: Date) {
+    return moment(date).format('YYYYMMDD');
   }
 };
