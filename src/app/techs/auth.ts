25b858fedbb2674126b7bc017b2dc32bddab46a1
Niels Maerten <niels@maerten.be>
2016-12-03 16:08:02 +0100

    Add authservice

--- src/app/techs/auth.ts
+++ src/app/techs/auth.ts
@@ -1,17 +1,43 @@
 /// <reference path="../../../typings/index.d.ts" />
 import * as firebase from 'firebase';
+import { FirebaseService } from './firebase';
 
 export class AuthService {
-  private database: firebase.database.Database;
+  private auth: firebase.auth.Auth;
+  private $q: angular.IQService;
 
 
   /** @ngInject */
-  constructor() {
-    //
+  constructor(FirebaseService: FirebaseService, $q: angular.IQService) {
+    this.auth = FirebaseService.getAuth();
+    this.$q = $q;
+
+    /*this.auth.onAuthStateChanged(user => {
+      if (!user) {
+        this.signIn(); // todo: redirect to sign in page instead?
+      }
+    });*/
   }
 
   public getUserId() {
-    // todo
-    //throw 'not implemented';
+    return this.isAuthenticated() ? this.auth.currentUser.uid : null;
+  }
+
+  public isAuthenticated() {
+    return this.auth.currentUser != null;
+  }
+
+  public signIn(): angular.IPromise<any> {
+    let deferred = this.$q.defer();
+    this.auth.onAuthStateChanged(user => {
+      if (!user) {
+        let provider = new firebase.auth.GoogleAuthProvider();
+        this.auth.signInWithRedirect(provider);
+      } else {
+        console.log('1st hit');
+        deferred.resolve(user);
+      }
+    });
+    return deferred.promise;
   }
 };
