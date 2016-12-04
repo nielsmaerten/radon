4d905808efe509af6fb848dcf8840093860e35bc
Niels Maerten <niels@maerten.be>
2016-12-05 00:40:31 +0100

    Solidify storage tests

--- src/app/services/storage.spec.ts
+++ src/app/services/storage.spec.ts
@@ -9,6 +9,7 @@ import { AuthService } from './auth';
 import { PlainStory } from '../model/radon';
 
 describe('storage service', () => {
+  let testDate = new Date(1880, 10, 10);
   beforeEach(done => {
     angular
       .module('app')
@@ -19,15 +20,25 @@ describe('storage service', () => {
     angular.mock.module('app');
 
     angular.mock.inject((EncryptionService: EncryptionService, AuthService: AuthService, StorageService: StorageService) => {
-      let plainStory = new PlainStory(new Date(), 'This is a test');
+      let plainStory = new PlainStory(testDate, 'This is a test');
       EncryptionService.loadEncryptionKey('testPassword');
       this.exampleStory = EncryptionService.encryptStory(plainStory);
       AuthService.signIn().then(done);
     });
   });
 
+  it('should save a story to firebase', done => angular.mock.inject((StorageService: StorageService) => {
+    StorageService.fetchStory(testDate).then(() => {
+      fail('A story already exists at date ' + testDate + '. No story should exist here before tests.');
+    }).catch(error => {
+      expect(error).toBeDefined();
+      StorageService.saveStory(this.exampleStory);
+      done();
+    });
+  }));
+
   it('should fetch a story from firebase', done => angular.mock.inject((StorageService: StorageService) => {
-    StorageService.fetchStory(new Date()).then((story) => {
+    StorageService.fetchStory(testDate).then((story) => {
       expect(story).toBeDefined();
       done();
     }).catch(error => {
@@ -35,11 +46,12 @@ describe('storage service', () => {
      });
   }));
 
-  it('should save a story to firebase', angular.mock.inject((StorageService: StorageService) => {
-    StorageService.saveStory(this.exampleStory);
-  }));
-
-  xit('should delete a saved story from firebase', angular.mock.inject((StorageService: StorageService) => {
-    //
+  it('should delete a saved story from firebase', done => angular.mock.inject((StorageService: StorageService) => {
+    StorageService.deleteStory(testDate).then(() => {
+      StorageService.fetchStory(testDate).catch(error => {
+        expect(error).toBeDefined();
+        done();
+      });
+    });
   }));
 });
