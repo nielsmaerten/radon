c8901a88969185777d505395d90aaa21f67bb064
Niels Maerten <niels@maerten.be>
2016-12-04 21:00:19 +0100

    Add header and banner to main component

--- src/app/main.spec.ts
+++ src/app/main.spec.ts
@@ -15,7 +15,7 @@ describe('main component', () => {
   it('should render the header, title, techs and footer', angular.mock.inject(($rootScope: ng.IRootScopeService, $compile: ng.ICompileService) => {
     const element = $compile('<app></app>')($rootScope);
     $rootScope.$digest();
-    expect(element.find('fountain-header').length).toEqual(1);
+    expect(element.find('app-header').length).toEqual(1);
     expect(element.find('fountain-title').length).toEqual(1);
     expect(element.find('fountain-techs').length).toEqual(1);
     expect(element.find('fountain-footer').length).toEqual(1);
