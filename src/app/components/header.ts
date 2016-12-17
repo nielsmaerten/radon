1259b763b9c69c5443e3577adad0e9ff9449e1a0
Niels Maerten <niels@maerten.be>
2016-12-17 23:30:57 +0100

    Create header controller

--- src/app/components/header.ts
+++ src/app/components/header.ts
@@ -1,3 +1,24 @@
+import { AuthService } from '../../app/services/auth';
+declare var $: any;
+class HeaderController {
+  private AuthService: AuthService;
+
+  /** @ngInject */
+  constructor(AuthService: AuthService) {
+    this.AuthService = AuthService;
+
+    $('#navPanel').panel({
+        delay: 500,
+        hideOnClick: true,
+        hideOnSwipe: true,
+        resetScroll: true,
+        resetForms: true,
+        side: 'left'
+      });
+  }
+}
+
 export const header: angular.IComponentOptions = {
-  template: require('./header.html')
+  template: require('./header.html'),
+  controller: HeaderController
 };
