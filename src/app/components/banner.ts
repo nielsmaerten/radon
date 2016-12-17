54709eecf6ccc3d4cc71f0e584bc294057448e2d
Niels Maerten <niels@maerten.be>
2016-12-17 23:32:13 +0100

    Create banner and banner controller

--- src/app/components/banner.ts
+++ src/app/components/banner.ts
@@ -1,3 +1,13 @@
+import { AuthService } from '../services/auth';
+class BannerController {
+  private AuthService: AuthService;
+
+  /** @ngInject */
+  constructor(AuthService: AuthService) {
+    this.AuthService = AuthService;
+  }
+}
 export const banner: angular.IComponentOptions = {
-  template: require('./banner.html')
+  template: require('./banner.html'),
+  controller: BannerController
 };
