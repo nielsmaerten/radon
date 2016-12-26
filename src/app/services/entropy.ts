e4902aa857219fd49c31a3965af6203d53938fcc
Niels Maerten <niels@maerten.be>
2016-12-26 21:55:52 +0100

    Add entropyService - Continue here, fix tests first

---
+++ src/app/services/entropy.ts
@@ -0,0 +1,14 @@
+/// <reference path="../../../typings/index.d.ts" />
+
+export class EntropyService {
+
+
+  /** @ngInject */
+  constructor() {
+    //
+  }
+
+  public generateSalt(): Q.Promise<string> {
+    throw 'not implemented';
+  }
+};
