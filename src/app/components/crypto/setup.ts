9143b3b4fe9ce4106e0e65be19958bd9c883d6a6
Niels Maerten <niels@maerten.be>
2017-01-05 23:41:41 +0100

    Add cryptosetup controller

--- src/app/components/crypto/setup.ts
+++ src/app/components/crypto/setup.ts
@@ -1,3 +1,15 @@
+class CryptoSetupController {
+  /** @ngInject */
+  constructor() {
+    //
+  }
+
+  public setPassphrase() {
+    alert('');
+  }
+}
+
 export const cryptoSetup: angular.IComponentOptions = {
-  template: require('./setup.html')
+  template: require('./setup.html'),
+  controller: CryptoSetupController
 };
