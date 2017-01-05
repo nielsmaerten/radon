104f7b7eae671eb920fdf7f946f5ec1ab947a9ec
Niels Maerten <niels@maerten.be>
2017-01-06 00:26:09 +0100

    Pass cryptoSetup tests

--- src/app/components/crypto/setup.ts
+++ src/app/components/crypto/setup.ts
@@ -1,11 +1,31 @@
+import { EncryptionService } from '../../services/encryption';
+import { StorageService } from '../../services/storage';
+import { AuthService } from '../../services/auth';
+
 class CryptoSetupController {
+  public passphrase: string;
+  public passphraseConfirm: string;
+  private StorageService: StorageService;
+  private EncryptionService: EncryptionService;
+  private $state: ng.ui.IStateService;
+
   /** @ngInject */
-  constructor() {
-    //
+  constructor(StorageService: StorageService, EncryptionService: EncryptionService, $state: ng.ui.IStateService, AuthService: AuthService) {
+    this.EncryptionService = EncryptionService;
+    this.StorageService = StorageService;
+    this.$state = $state;
+    if (!AuthService.isAuthenticated) {
+      $state.go('app.home');
+    }
   }
 
   public setPassphrase() {
-    alert('');
+    if (this.passphrase === this.passphraseConfirm) {
+      this.StorageService.setSalt().then(() => {
+        this.EncryptionService.loadEncryptionKey(this.passphrase);
+        this.$state.go('app.calendar');
+      });
+    }
   }
 }
 
