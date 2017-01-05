54ced9fb180846f817050629f65bb2a9935823b8
Niels Maerten <niels@maerten.be>
2017-01-06 00:50:11 +0100

    Catch incorrect states on setup and banner components

--- src/app/components/banner.ts
+++ src/app/components/banner.ts
@@ -1,10 +1,24 @@
 import { AuthService } from '../services/auth';
+import { EncryptionService } from '../services/encryption';
+import { StorageService } from '../services/storage';
+
 class BannerController {
   private AuthService: AuthService;
 
   /** @ngInject */
-  constructor(AuthService: AuthService) {
+  constructor(AuthService: AuthService, EncryptionService: EncryptionService, StorageService: StorageService, $state: ng.ui.IStateService) {
     this.AuthService = AuthService;
+    this.AuthService.authPromise.then(() => {
+      StorageService.onSaltSet(() => {
+        if (!EncryptionService.hasSalt()) {
+          $state.go('app.cryptoSetup');
+        } else if (EncryptionService.isReady()) {
+          $state.go('app.calendar');
+        } else {
+          $state.go('app.cryptoUnlock');
+        }
+      });
+    });
   }
 }
 export const banner: angular.IComponentOptions = {
--- src/app/components/crypto/setup.ts
+++ src/app/components/crypto/setup.ts
@@ -15,7 +15,18 @@ class CryptoSetupController {
     this.StorageService = StorageService;
     this.$state = $state;
     if (!AuthService.isAuthenticated) {
+      // the app either has not fully initialized, or I'm simply not yet authenticated.
+      // either way, I should not be creating a key right now: redirect to the home route
       $state.go('app.home');
+    } else {
+      // i am authenticated, but has the salt been set yet?
+      StorageService.onSaltSet(() => {
+        if (EncryptionService.hasSalt()) {
+          // a salt has been set, and i cannot set it again: redirect to home
+          // from home, I will either be redirected to crypto.Unlock or to the calendar
+          $state.go('app.home');
+        }
+      });
     }
   }
 
