4d24dd47b7001e0f9740ac33b0b60db07d7813c1
Niels Maerten <niels@maerten.be>
2017-01-06 01:12:46 +0100

    Update tests

--- src/app/components/banner.spec.ts
+++ src/app/components/banner.spec.ts
@@ -2,9 +2,13 @@
 
 import * as angular from 'angular';
 import 'angular-mocks';
+import 'angular-ui-router';
 import * as $ from 'jquery';
 import { banner } from './banner';
 import { AuthService } from '../services/auth';
+import { EncryptionService } from '../services/encryption';
+import { StorageService } from '../services/storage';
+import { EntropyService } from '../services/entropy';
 import { FirebaseService } from '../services/firebase';
 
 describe('banner component', () => {
@@ -13,8 +17,11 @@ describe('banner component', () => {
       .module('appBanner', ['app/components/banner.html'])
       .service('AuthService', AuthService)
       .service('FirebaseService', FirebaseService)
+      .service('EncryptionService', EncryptionService)
+      .service('StorageService', StorageService)
+      .service('EntropyService', EntropyService)
       .component('appBanner', banner);
-    angular.mock.module('appBanner');
+    angular.mock.module('appBanner', 'ui.router');
   });
 
   it('should render a title', angular.mock.inject(($rootScope: ng.IRootScopeService, $compile: ng.ICompileService) => {
--- src/app/components/crypto/unlock.spec.ts
+++ src/app/components/crypto/unlock.spec.ts
@@ -2,21 +2,33 @@
 
 import * as angular from 'angular';
 import 'angular-mocks';
+import 'angular-ui-router';
 import * as $ from 'jquery';
 import { cryptoUnlock } from './unlock';
+import { AuthService } from '../../services/auth';
+import { EncryptionService } from '../../services/encryption';
+import { StorageService } from '../../services/storage';
+import { EntropyService } from '../../services/entropy';
+import { FirebaseService } from '../../services/firebase';
 
 describe('crypto unlock component', () => {
   beforeEach(() => {
     angular
       .module('appCryptoUnlock', ['app/components/crypto/unlock.html'])
-      .component('appCryptoUnlock', cryptoUnlock);
-    angular.mock.module('appCryptoUnlock');
+      .component('appCryptoUnlock', cryptoUnlock)
+      .service('AuthService', AuthService)
+      .service('FirebaseService', FirebaseService)
+      .service('EncryptionService', EncryptionService)
+      .service('StorageService', StorageService)
+      .service('EntropyService', EntropyService)
+      ;
+    angular.mock.module('appCryptoUnlock', 'ui.router');
   });
 
-  xit('should render a password field', angular.mock.inject(($rootScope: ng.IRootScopeService, $compile: ng.ICompileService) => {
+  it('should render a password field', angular.mock.inject(($rootScope: ng.IRootScopeService, $compile: ng.ICompileService) => {
     const element = $($compile('<app-crypto-unlock></app-crypto-unlock>')($rootScope));
     $rootScope.$digest();
     let selector = 'input[type="password"]';
-    //expect(element.find(selector).length).toBe(1);
+    expect(element.find(selector).length).toBe(1);
   }));
 });
