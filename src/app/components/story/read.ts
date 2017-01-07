ec81494847a80cde3b59a57d46b3f860143df3bd
Niels Maerten <niels@maerten.be>
2017-01-07 22:26:14 +0100

    Add navigation on top of story read view

--- src/app/components/story/read.ts
+++ src/app/components/story/read.ts
@@ -1,18 +1,21 @@
 import { PlainStory } from '../../model/radon';
 import { StorageService } from '../../services/storage';
 import { EncryptionService } from '../../services/encryption';
+import * as moment from 'moment';
 
 class StoryReadController {
   private moment: moment.MomentStatic = require('moment');
   private story: PlainStory;
   private date: Date;
   private noStoryAvailable: boolean;
+  private $state: ng.ui.IStateService;
 
   /** @ngInject */
   constructor($state: ng.ui.IStateService, StorageService: StorageService, EncryptionService: EncryptionService, $scope: ng.IRootScopeService) {
     if (!EncryptionService.isReady()) {
       $state.go('app.home');
     } else {
+      this.$state = $state;
       this.date = this.moment(($state.params as any).storyDate, 'YYYYMMDD').toDate();
       StorageService.fetchStory(this.date)
         .then(encryptedStory => {
@@ -23,6 +26,11 @@ class StoryReadController {
           $scope.$apply();
         });
     }
+  }
+
+  moveStory(d: number) {
+    let newDate = moment(this.date).add('day', d).format('YYYYMMDD');
+    this.$state.go('app.storyRead', { storyDate: newDate });
   }
 }
 
