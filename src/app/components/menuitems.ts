f6d08dd81081b3b221833cfe6ddbb6e1b068ae2b
Niels Maerten <niels@maerten.be>
2017-01-07 23:43:08 +0100

    Add delete, edit, save and discard buttons to menu

--- src/app/components/menuitems.ts
+++ src/app/components/menuitems.ts
@@ -1,11 +1,32 @@
 import { AuthService } from '../services/auth';
+import * as PubSub from 'pubsub-js';
 
 class MenuitemsController {
+  public isEditingStory: boolean;
+  public isReadingStory: boolean;
   private AuthService: AuthService;
+  private $state: ng.ui.IStateService;
 
   /** @ngInject */
-  constructor(AuthService: AuthService) {
+  constructor(AuthService: AuthService, $state: ng.ui.IStateService) {
+    this.$state = $state;
     this.AuthService = AuthService;
+  }
+
+  saveStory() {
+    PubSub.publish('story', 'save');
+  }
+
+  editStory() {
+    PubSub.publish('story', 'edit');
+  }
+
+  deleteStory() {
+    PubSub.publish('story', 'delete');
+  }
+
+  discardStory() {
+    PubSub.publish('story', 'discard');
   }
 }
 
